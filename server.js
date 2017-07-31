54be4ac9b21b507106de229e9f52729d060acd43
root <root@art-fcp-master.lsd.ufcg.edu.br>
2017-07-31 11:23:54 -0300

    Add v1 branch

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
+<<<<<<< f236c8374ab808cd30f5ab464bd9bbd6e096b18e
   response.end('Hello');
+=======
+  response.end('Hello LSD, I am V1');
+>>>>>>> Add v1 branch
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
