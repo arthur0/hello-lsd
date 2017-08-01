474f4dbfe5ec970d9da02f55f149cc6c9fcf6d0a
root <root@art-fcp-master.lsd.ufcg.edu.br>
2017-08-01 14:30:33 -0300

    Add a breakline

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
+<<<<<<< 54be4ac9b21b507106de229e9f52729d060acd43
 <<<<<<< f236c8374ab808cd30f5ab464bd9bbd6e096b18e
   response.end('Hello');
 =======
   response.end('Hello LSD, I am V1');
 >>>>>>> Add v1 branch
+=======
+  response.end('Hello LSD, I am V1\n');
+>>>>>>> Add a breakline
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
